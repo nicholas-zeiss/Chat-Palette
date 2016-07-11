a0b12da7432e4efb9e11b3e36964e0db282b25ee
superhashbros <superhashbros42@gmail.com>
2016-07-11 15:32:05 -0400

    fixed ridwan's server.js bullshit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 /**
-*     This is the server file that will serve up all of
-*     our front end pages based on which endpoints are
-*     being fired and will also handle any database manipulation
+ *   This is the server file that will serve up all of
+ *   our front end pages based on which endpoints are
+ *   being fired and will also handle any database manipulation
  */
 
 var express = require('express');
 var path = require('path');
-var db = require('./db.js')
+var db = require('./db.js');
 
 //Creates instance of express object
 var app = express();
@@ -15,35 +15,32 @@ var rootpath = path.normalize(__dirname + '/..');
 
 //this will serve up the hompage aka index
 app.get('/', function(req, res) {
-    console.log("Hello World");
-    console.log(rootpath);
-    // res.sendFile(path.join(rootpath+'/index.html'));
+  res.redirect('/chat'); 
 });
 
 //this will be used for login page
-app.get('/login', function(req, res) {
-    console.log("Hello World login");
-    console.log(rootpath);
+app.post('/login', function(req, res) {
+
+
+});
+
+app.post('/signup', function(req, res) {
+
 
 });
 
 //this will serve up the main chat page
 app.get('/chat', function(req, res) {
-    console.log("Hello World chat");
-    console.log(rootpath);
-    // alert("Hello World")
+
 
 });
 
-// app.get('/*',function(req,res){
-//   console.log(req.url)
-//
-//   if(req.url !== '/') res.sendFile(rootpath+req.url);
-// })
+app.post('/chat', function(req, res) {
 
+
+});
 
 var port = process.env.PORT || 8080;
-
-app.listen(port, function() {
-console.log("Listening on " + port);
+	app.listen(port, function() {
+	console.log("Listening on " + port);
 });
