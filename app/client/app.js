164e6375b20c3126365224a0c994168ef19cdc5d
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-09-22 02:08:42 -0400

    removed /login url as it is redundant with /

--- app/client/app.js
+++ app/client/app.js
@@ -1,32 +1,32 @@
 /**
- * Main module for Chat Palatte App
- */
+ *
+ *  Here we create our root level module, app, and configure the router
+ *
+**/
+
 
 angular.module('app', [
-  'app.auth',
-  'app.services',
-  'app.chat',
-  'ngRoute'
+	'app.auth',
+	'app.services',
+	'app.chat',
+	'ngRoute'
 ])
-.config(function($routeProvider, $httpProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'auth/login.html',
-      controller: 'AuthController'
-    })
-    .when('/signup', {
-      templateUrl: 'auth/signup.html',
-      controller: 'AuthController'
-    })
-    .when('/login', {
-      templateUrl: 'auth/login.html',
-      controller: 'AuthController'
-    })
-    .when('/chat', {
-      templateUrl: 'chat/chat.html',
-      controller: 'ChatController'
-    })
-    .otherwise({
-      redirectTo:'/'
-    });
-});
+	.config( $routeProvider => {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'auth/login.html',
+				controller: 'AuthController'
+			})
+			.when('/signup', {
+				templateUrl: 'auth/signup.html',
+				controller: 'AuthController'
+			})
+			.when('/chat', {
+				templateUrl: 'chat/chat.html',
+				controller: 'ChatController'
+			})
+			.otherwise({
+				redirectTo:'/'
+			});
+	});
+
